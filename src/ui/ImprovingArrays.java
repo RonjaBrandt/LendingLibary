73de5dc931539ec34d6a3aefd3fc0145c76afdbd
brandtronja@gmail.com <Esra.2016!>
2018-10-09 16:16:19 +0200

    Update 2018-10-09 16:16

--- src/ui/ImprovingArrays.java
+++ src/ui/ImprovingArrays.java
@@ -1,4 +1,29 @@
 package ui;
 
+import java.util.HashSet;
+
 public class ImprovingArrays {
+    public static void main(String[] args) {
+
+
+        HashSet<String> myHashSet = new HashSet<>();
+
+
+        myHashSet.add("first item");
+        myHashSet.add("second item");
+        myHashSet.add("third item");
+        myHashSet.add("fourth item");
+
+
+        System.out.println(myHashSet.size());
+
+        myHashSet.remove("second item");
+
+        System.out.println(myHashSet.size());
+    }
+
+
+
+
+
 }
